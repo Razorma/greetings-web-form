5bd7893701ba1e4d53d7619613e09f20da3fcf8b
bheka <bheka.bj@gmail.com>
2023-08-04 22:49:53 +0200

    added connection to database

---
+++ database.js
@@ -0,0 +1,127 @@
+
+import pgPromise from 'pg-promise';
+import pg from 'pg';
+
+const pgp = pgPromise();
+
+
+const connectionString = process.env.DATABASE_URL || 'postgres://bheka:OByrOSiZ7tqz1mAzx72ukmRZNAPr0Iol@dpg-cj5qva2cn0vc73flmoq0-a.oregon-postgres.render.com/razorma_r4tr';
+const ssl = { rejectUnauthorized: false }
+
+const db = pgp({ connectionString, ssl });
+
+const pool = new pg.Pool({
+  connectionString: connectionString,
+  ssl: {
+    rejectUnauthorized: false // Ignore SSL certificate validation for now 
+  }
+});
+
+async function createUsersTable() {
+  try {
+    const createTableQuery = `
+    CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      username VARCHAR(255) NOT NULL,
+      greeted_count INTEGER DEFAULT 0
+    );
+  `;
+    await db.none(createTableQuery);
+    console.log('Table "users" created successfully.');
+
+    const checkConstraintQuery = `
+      SELECT constraint_name
+      FROM information_schema.table_constraints
+      WHERE table_name = 'users' AND constraint_type = 'UNIQUE';
+    `;
+    const existingConstraint = await db.oneOrNone(checkConstraintQuery);
+
+    if (!existingConstraint) {
+      const addUniqueConstraintQuery = `
+        ALTER TABLE users ADD CONSTRAINT unique_username UNIQUE (username);
+      `;
+      await db.none(addUniqueConstraintQuery);
+      console.log('Unique constraint added to "username" column.');
+    } else {
+      console.log('Unique constraint already exists.');
+    }
+
+  } catch (error) {
+    console.error('Error creating table:', error.message);
+  }
+}
+
+async function addUser(username) {
+  try {
+    const insertQuery = `
+      INSERT INTO users (username, greeted_count)
+      VALUES ($1, 1)
+      ON CONFLICT (username) DO UPDATE
+      SET greeted_count = users.greeted_count + 1
+      RETURNING id;
+    `;
+    const client = await pool.connect();
+    const result = await db.oneOrNone(insertQuery, [username]);
+    client.release();
+    if (result) {
+      console.log(`User "${username}" greeted. Greeted count: ${result.greeted_count}`);
+    } else {
+      console.log(`User "${username}" already exists. Greeting count updated.`);
+    }
+  } catch (error) {
+    console.error('Error adding user:', error.message);
+  }
+}
+
+
+
+async function getUsers() {
+    try {
+      const selectQuery = `
+        SELECT * FROM users;
+      `;
+      const users = await db.any(selectQuery);
+      console.log('All users:');
+      console.log(users);
+      return users
+    } catch (error) {
+      console.error('Error getting users:', error.message);
+    }
+  }
+  async function getGreetedUsersCount() {
+    try {
+      const selectQuery = `
+        SELECT COUNT(*) FROM users;
+      `;
+      const countResult = await db.one(selectQuery);
+      const totalCount = countResult.count;
+      console.log(`Total users greeted: ${totalCount}`);
+      return totalCount
+    } catch (error) {
+      console.error('Error getting greeted users count:', error.message);
+    }
+  }
+  
+  async function removeAllUsers() {
+    try {
+      const deleteQuery = `
+        DELETE FROM users;
+      `;
+      await db.none(deleteQuery);
+      console.log('All users removed successfully.');
+    } catch (error) {
+      console.error('Error removing users:', error.message);
+    }
+  }
+  
+export {
+  createUsersTable,
+  getUsers,
+  addUser,
+  getGreetedUsersCount,
+  removeAllUsers,
+  pool 
+};
+
+export { db };
+
--- index.js
+++ index.js
@@ -2,10 +2,72 @@ import express from "express";
 import { engine } from 'express-handlebars';
 import bodyParser from "body-parser";
 import greet from "./greet.js";
+import pkg from 'pg';
+const { Client } = pkg;
+import flash from 'express-flash';
+import session from 'express-session';
 
+
+const { Pool } = pkg;
+import fs from 'fs';
+import {
+  createUsersTable,
+  getUsers,
+  addUser,
+  getGreetedUsersCount,
+  removeAllUsers
+} from './database.js';
+
+
+const connectionString = process.env.DATABASE_URL||'postgres://bheka:OByrOSiZ7tqz1mAzx72ukmRZNAPr0Iol@dpg-cj5qva2cn0vc73flmoq0-a.oregon-postgres.render.com/razorma_r4tr';
+
+const client = new Client({
+  connectionString: connectionString,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+
+client.connect()
+  .then(() => {
+    console.log('Connected to the database');
+  })
+  .catch((err) => {
+    console.error('Error connecting to the database:', err.message);
+  });
+  import { pool } from './database.js';
+
+import { db } from './database.js';
+async function connectToDatabase() {
+  try {
+    await db.connect();
+    console.log('Connected to the database');
+  } catch (error) {
+    console.error('Error connecting to the database:', error.message);
+  }
+}
+async function main() {
+  // Create the 'users' table
+  await createUsersTable();
+  // await addUser();
+  
+  await connectToDatabase();
+  // await getGreetedUsersCount();
+  // await removeAllUsers()
+  await getUsers();
+  // db.$pool.end();
+}
 
 let app = express();
 const greeting = greet()
+
+app.use(session({ 
+  secret: 'Razorma', 
+  resave: false,
+  saveUninitialized: true,
+}));
+app.use(flash());
 
 // Setup the Handlebars view engine
 app.engine('handlebars', engine());
@@ -15,39 +77,87 @@ app.set('views', './views');
 app.use(express.static('public'));
 
 
+
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 
-app.get('/', function (req, res) {
-  
-    res.render('home',{
-      name:greeting.greetName(),
-      language:greeting.getLanguageGreeting(),
-      counter:greeting.greetNumber(),
-      errorName:greeting.errorName(),
-      errorLang:greeting.errorLang(),
+app.get('/', async function (req, res) {
+  try {
+    const greetedCount = await getGreetedUsersCount(); 
+
+    res.render('home', {
+      name: greeting.greetName(),
+      language: greeting.getLanguageGreeting(),
+      counter: greetedCount, 
+      errorName: greeting.errorName(),
+      errorLang: greeting.errorLang(),
+      // error
     });
+  } catch (error) {
+    console.error('Error:', error.message);
+    res.status(500).send('Internal Server Error');
+  }
 });
-  
-app.post("/greetings", function (req, res) {
+
+
+app.post("/greetings", async function (req, res) {
   const letterRegex = /^[a-zA-Z ]*$/
   if(req.body.name===""||req.body.language===undefined){
     
     greeting.getName("")
     greeting.setLanguageGreeting("")
     greeting.greetName()
+    
   }else  if(letterRegex.test(req.body.name)){
     greeting.getName(req.body.name)
     greeting.setLanguageGreeting(req.body.language)
 
+    try {
+      await addUser(req.body.name);
+      await getUsers();
+      // await getGreetedUsersCount();
+      req.flash('success', 'User added successfully.');
+    } catch (error) {
+      console.error('Error adding user:', error.message);
+    }
 
   } 
     res.redirect("/")
 });
+app.get("/greeted", async function (req, res) {
+  try {
+    const names = await getUsers(); 
+
+    res.render('greeted', { names }); 
+  } catch (error) {
+    console.error('Error getting users:', error.message);
+    res.status(500).send('Internal Server Error');
+  }
+});
+app.get("/counter/:name", async function (req, res) {
+  const requestedName = req.params.name;
+  try {
+    const nameArray = await getUsers();
+    const matchingUsers = nameArray.filter(user => user.username === requestedName);
+    res.render('counter', { names: matchingUsers });
+  } catch (error) {
+    console.error('Error getting users:', error.message);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
 
   let PORT = process.env.PORT || 3012;
   
   app.listen(PORT, function(){
     console.log('App starting on port', PORT);
   });
+
+
+
+
+main().catch((error) => {
+  console.error('An error occurred:', error);
+});
